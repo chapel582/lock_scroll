a39ce0f7f812d87fd5d825ef67190223f6c19a50
Trevor Apple <trappleco@gmail.com>
2022-09-19 16:12:51 -0700

    ruler kicks out to the nearest 80 + 4n value

---
+++ README.md
@@ -0,0 +1,6 @@
+# Installation
+1. Install https://github.com/pyrrho/sublime-horizontal-scroll
+2. Move the lock_scroll and keymap to a directory called lock_hscroll in your sublime packages.
+
+On windows, the path looks something like this
+`C:\Users\ChapelEver\AppData\Roaming\Sublime Text 3\Packages`
--- lock_scroll.py
+++ lock_scroll.py
@@ -4,16 +4,21 @@ from sublime import Region, DRAW_NO_OUTLINE, DRAW_EMPTY
 
 sync_running = False
 
-def set_ruler_offset_from_visible(view, additional_offset):
-	"""
-	additional_offset:
-		Additional offset from the visible region. Needed since viewport
-		position doesn't update immdediately 
-	"""
+def get_visibility_offset(view):
 	visible_region = view.visible_region()
+	return  visible_region.begin() - view.lines(visible_region)[0].begin()
+
+
+def set_ruler_offset_from_visible(view):
 	ruler_pos = 80
-	offset = visible_region.begin() - view.lines(visible_region)[0].begin()
-	view.settings().set('rulers', [ruler_pos + offset + additional_offset])
+	offset = get_visibility_offset(view)
+	# NOTE: round the ruler to the nearest multiple of 4 added to 80
+	print("offset before", offset)
+	offset += (4 - offset) % 4
+	print("offset after", offset)
+	actual_ruler_pos = ruler_pos + offset
+	print("ruler pos", actual_ruler_pos)
+	view.settings().set('rulers', [actual_ruler_pos])
 
 class ScrollListener(sublime_plugin.EventListener):
 	old_pos = None
@@ -31,16 +36,9 @@ class ScrollListener(sublime_plugin.EventListener):
 				# NOTE: when locked, we get the new y pos, but not the new xpos
 				view.set_viewport_position((self.old_pos[0], new_pos[1]), False)
 				additional_offset = 0
-			else:
-				# NOTE: check if we are moving left or right
-				tab_size = view.settings().get('tab_size', 4)
-				if new_pos[0] > self.old_pos[0]:
-					additional_offset = tab_size
-				else:
-					additional_offset = -1 * tab_size
 
 			# NOTE: when locked, update the ruler with scroll width commands
-			set_ruler_offset_from_visible(view, additional_offset)
+			set_ruler_offset_from_visible(view)
 
 class LockScrollCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -50,6 +48,6 @@ class LockScrollCommand(sublime_plugin.TextCommand):
 		sync_running = current_state
 
 		if sync_running:
-			set_ruler_offset_from_visible(self.view, 0)
+			set_ruler_offset_from_visible(self.view)
 		else:
 			self.view.settings().set('rulers', [])
